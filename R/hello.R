7aa29a69d2f2f669492abf41a54290338e0eace2
suleymantaspinar <sl.taspinar@gmail.com>
2020-03-30 21:15:05 +0300

    External data files moved to  extdata folder

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,15 @@
+meldaJs <- function() {
+  includeScript(system.file("extdata/bundle.js", package = "meldakb"))
+}
+
+meldaHtml <- function(){
+  includeHTML(system.file("extdata/template.html",package = "meldakb"))
+}
+
 meldaKB <- function(){
   library(shiny)
 
-  ui <- htmlTemplate("template.html",
+  ui <- htmlTemplate(meldaHtml(),
                      exitButton =tags$button(
                        id = 'Close',
                        type = "button",
@@ -9,10 +17,9 @@ meldaKB <- function(){
                        onclick = "setTimeout(function(){window.close();window.parent.postMessage('disconnected', '*');},500);",
                        "Close window"
                      ),
-                     bundle = includeScript("www/node_modules/knowledge-base/dist/bundle.js")
+                     bundle = includeScript(meldaJs())
   )
 
   server <- function(input, output, session) {}
   shinyApp(ui = ui, server = server)
 }
-
