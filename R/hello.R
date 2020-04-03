f9f080a56bb864bd3a055f565666d8dc1b27896f
suleymantaspinar <sl.taspinar@gmail.com>
2020-04-03 06:14:35 +0300

    Load more buttons added

--- R/hello.R
+++ R/hello.R
@@ -1,72 +1,25 @@
-library(shiny)
-library(miniUI)
-library(DT)
-library(httr)
+meldaJs <- function() {
+  system.file("extdata/bundle.js", package = "meldakb")
+}
+
+meldaHtml <- function(){
+  system.file("extdata/template.html", package = "meldakb")
+}
 
 meldaKB <- function(){
-  ui <- miniPage(
-        textInput("search","Search in Packages,Methods, Authors","",width = '100%'),
+  library(shiny)
 
-    miniTabstripPanel(
-      miniTabPanel(id = "pkg","Packages",icon = icon("table"),
-                   miniContentPanel(
-                     (DT::dataTableOutput("packagesTable"))),
-      ),
-      miniTabPanel("Method",icon = icon("table"),
-                   miniContentPanel(
-                     (DT::dataTableOutput("methodsTable")))
-      ),
-      miniTabPanel("Author", icon = icon("table"),
-                   miniContentPanel(
-                     ((DT::dataTableOutput("authorsTable")))
-                   ))
-    )
+  ui <- htmlTemplate(meldaHtml(),
+                     exitButton =tags$button(
+                       id = 'Close',
+                       type = "button",
+                       class = "btn action-button",
+                       onclick = "setTimeout(function(){window.close();window.parent.postMessage('disconnected', '*');},500);",
+                       "Close window"
+                     ),
+                     bundle = includeScript(meldaJs())
   )
 
-  server <- function(input, output, session) {
-
-    url <- "https://kbdev.melda.io/"
-    rv <- reactiveValues()
-    rv$methods = data.frame()
-    rv$authors = data.frame()
-    rv$packages = data.frame()
-    rv$packageName = ''
-
-    observe({
-      req(input$search)
-      res <- httr::GET(paste0(url,"search?q=",URLencode(input$search),"&size=100"))
-      resAuthor <-  httr::GET(paste0(url,"search?q=",URLencode(input$search),"&size=100"))
-
-      rv$packages <- data.frame(name = sapply(content(res)$packages,function(x) x$name )
-                                ,description = sapply(content(res)$packages,function(x) x$description ),
-                                version = sapply(content(res)$packages,function(x) x$version ))
-
-      rv$methods = data.frame(name = sapply(content(res)$methods,function(x) x$name),
-                              title = sapply(content(res)$methods,function(x) x$title),
-                              description = sapply(content(res)$methods,function(x) x$description))
-
-      rv$authors = data.frame(name = sapply(content(resAuthor)$packages,function(x) x$author )
-                              ,Package = sapply(content(resAuthor)$packages,function(x) x$name ))
-
-    })
-
-    output$packagesTable <- DT::renderDataTable({
-      req(rv$packages)
-      rv$packages
-    })
-
-    output$methodsTable <- DT::renderDataTable({
-      req(rv$methods)
-      datatable(
-        rv$methods, selection =  "none", class="cell-border strip hover"
-      ) %>% formatStyle(0, cursor = 'pointer')
-
-    })
-
-    output$authorsTable <- DT::renderDataTable({
-      req(rv$authors)
-      rv$authors
-    })
-  }
-  runGadget(shinyApp(ui, server),  viewer = paneViewer())
+  server <- function(input, output, session) {}
+  shinyApp(ui = ui, server = server)
 }
